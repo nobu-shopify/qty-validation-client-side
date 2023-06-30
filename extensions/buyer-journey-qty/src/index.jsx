d68a6813399aadd5c5be86db6404cb9fdb58ea9a
Nobu Hayashi <nobu.hayashi@shopify.com>
2023-06-30 09:58:54 +0900

    max qty to purchase from product metafield

--- extensions/buyer-journey-qty/src/index.jsx
+++ extensions/buyer-journey-qty/src/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   useExtensionApi,
   render,
@@ -14,9 +14,13 @@ render('Checkout::Dynamic::Render', () => <App />);
 
 function App() {
   const {extensionPoint} = useExtensionApi();
+  const { query } = useExtensionApi();
+
   const translate = useTranslate();
 
   const [validationError, setValidationError] = useState("");
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const canBlockProgress = useExtensionCapability("block_progress");
   const label = canBlockProgress ? "LIMIT QTY" : "NO LIMIT";
@@ -24,23 +28,67 @@ function App() {
   const cartLines = useCartLines();
 
   let qty = 0;
-  let not_for_sale = false;
+  let id = "";
+
+  // Get max qty product from cart
   for (const cartLine of cartLines) {
     if (cartLine.quantity > qty) {
       qty = cartLine.quantity;
-    }
-    if(cartLine.merchandise.product.productType === "NOT_FOR_SALE") {
-      not_for_sale = true;
+      id = cartLine.merchandise.product.id;
     }
   }
+
+  // On initial load, fetch the product metafield
+  useEffect(() => {
+    // Set the loading state to show some UI if you're waiting
+    setLoading(true);
+
+    // Use `query` api method to send graphql queries to the Storefront API
+    query(
+      `query {
+        product(id: "${id}") {
+          id
+          title
+          metafield(namespace: "custom", key: "max_quantity_per_checkout") {
+            value
+          }
+        }
+      }`
+    )
+    .then(({data}) => {
+      // Set the `product` object so that you can reference the items
+      setProduct(data.product);
+    })
+    .catch((error) => console.error(error))
+    .finally(() => setLoading(false));
+  }, []);
+  
   console.log(`qty: ${qty}`);
-  console.log(`not_for_sale: ${not_for_sale}`);
+  console.log(`ID: ${id}`);
+  console.log(`Product title: ${product?.title}`);
+  console.log(`Product max qty: ${product?.metafield.value}`);
+
+  const max_qty = (product ? product.metafield.value : 0);
+  console.log(`max qty: ${max_qty}`);
 
   // Use the `buyerJourney` intercept to conditionally block checkout progress
   // The ability to block checkout progress isn't guaranteed.
   // Refer to the "Check for the ability to block checkout progress" section for more information.
   useBuyerJourneyIntercept(({ canBlockProgress }) => {
 
+    // NOT_FOR_SALE
+    if (canBlockProgress && !isForSale()) {
+      return {
+        behavior: "block",
+        reason: `Cart contains NOT FOR SALE item(s)`,
+        perform: (result) => {
+          // If progress can be blocked, then set a validation error on the custom field
+          if (result.behavior === "block") {
+            setValidationError(`Cart contains NOT FOR SALE item(s)`);
+          }
+        },
+      };
+    }
     // Qty limit
     if (canBlockProgress && !isQtyOK()) {
       return {
@@ -50,19 +98,6 @@ function App() {
           // If progress can be blocked, then set a validation error on the custom field
           if (result.behavior === "block") {
             setValidationError(`TOO MANY ITEMS IN CART`);
-          }
-        },
-      };
-    }
-    // NOT_FOR_SALE
-    if (canBlockProgress && isNotForSale()) {
-      return {
-        behavior: "block",
-        reason: `Cart contains NOT FOR SALE item(s)`,
-        perform: (result) => {
-          // If progress can be blocked, then set a validation error on the custom field
-          if (result.behavior === "block") {
-            setValidationError(`Cart contains NOT FOR SALE item(s)`);
           }
         },
       };
@@ -78,10 +113,10 @@ function App() {
   });
 
   function isQtyOK() {
-    return qty <= 2;
+    return qty <= max_qty;
   }
-  function isNotForSale() {
-    return not_for_sale;
+  function isForSale() {
+    return max_qty > 0;
   }
 
   return (
